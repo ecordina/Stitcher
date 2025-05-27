bbbd68c75d1337b29c65a922263eb9ee4961d418
Dragonfly user <dragonflyuser@NYGENOME.ORG>
2025-05-27 12:05:25 -0400

    Added gitignore, formatted stitching, latest checks with and without FFC and max project passed on at least one dataset

--- stitching.sh
+++ stitching.sh
@@ -5,27 +5,27 @@
 #SBATCH --mem=50g
 
 # Arguments
-FOLDER="/gpfs/commons/home/ecordina/ecordina_innovation/dragonfly_stitching/Stitcher/data/tiles/"
-XML_FILE="/gpfs/commons/home/ecordina/ecordina_innovation/dragonfly_stitching/Stitcher/data/tiles/MP_Chao_250415-F1_cs_13.31.15.xml"
+FOLDER=""
+XML_FILE="Z:\FusionImages\2025-05-06\MP_2025410_E18-F2_14.03.23.xml"
 
 # Default Arguments
-OUTPUT_PATH="./data/output" #Save in folder where the bash script is
-SAVE_FORMAT="tiff" # default to 'tiff' if not specified
+OUTPUT_PATH="./data/output"        # Folder where to save the output
+SAVE_FORMAT="tiff" 		   # Default to 'tiff' if not specified, accepts tiff and zarrs
 
 # Optional flags
-IMAGE_PREFIX="" # In case the image prefix is different than the one in the metadata /!\, has to be complete as the code will look for image_prefix+"_F\d+"
-		# ie: For MP_20250411-C1_14.38.46_F000.ims, IMAGE_PREFIX = "MP_20250411-C1_14.38.46" 
-MAX_PROJECT="--max_project"  # add --max_project to Z-Max Projection
-OVERLAP="" #Leave Empty to scrap overlap from the xml
-REFINE_OVERLAP="--refine_overlap" # take it off if you want to force either the given overlap value or the overlap from the xml file
-CROP_OVERLAP="--crop_overlap" # leave empty to use max projection on the overlap, add --crop_overlap if you want to use crop overlap
-flat_field_path="/gpfs/commons/home/ecordina/ecordina_innovation/dragonfly_stitching/Stitcher/data/flat_field.tiff"
-NO_BACKGROUND=""          # leave empty to enable background subtraction
+IMAGE_PREFIX="" 		   # In case the image prefix is different than the one in the metadata /!\, has to be complete as the code will look for image_prefix+"^_F\d+$"
+				   # ie: For MP_20250411-C1_14.38.46_F000.ims, IMAGE_PREFIX = "MP_20250411-C1_14.38.46" 
+MAX_PROJECT="--max_project"        # Add --max_project to Z-Max Projection
+OVERLAP="" 			   # Leave Empty to scrap overlap from the xml, else specify the overlap
+REFINE_OVERLAP="--refine_overlap"  # Take it off if you want to force either the given overlap value or the overlap from the xml file
+CROP_OVERLAP="--crop_overlap"      # Leave empty to use max projection on the overlap, add --crop_overlap if you want to use crop overlap
+flat_field_path=""		   # Flat Field Image if you want to perform Flat-Field Correction, leave empty if not. Be careful that the Flat Field Tile has to be the same dimensions as the image tiles
+NO_BACKGROUND=""                   # Leave empty to enable background subtraction, else add --no_background_subtraction
 attrs="{'Instrument':'Dragonfly'}" # Attributes to attach to the zarr, only used when save_format == zarr
-logger="" # Add --disable_logger to disable logger
+logger="" 			   # Add --disable_logger to disable logger
 
 # Run the Python script
-python /gpfs/commons/home/ecordina/ecordina_innovation/dragonfly_stitching/Stitcher/src/stitching.py \
+python ./src/stitching.py \
   --folder "$FOLDER" \
   --xml_file "$XML_FILE" \
   --output_path "$OUTPUT_PATH" \
@@ -33,9 +33,10 @@ python /gpfs/commons/home/ecordina/ecordina_innovation/dragonfly_stitching/Stitc
   --attrs "$attrs" \
   --image_prefix "$IMAGE_PREFIX" \
   --flat_field_path "$flat_field_path" \
+  --save_format "$SAVE_FORMAT" \
   $MAX_PROJECT \
   $REFINE_OVERLAP \
   $CROP_OVERLAP \
   $NO_BACKGROUND \
   $logger \
-  --save_format "$SAVE_FORMAT"
+
